c37b79be6cbd1734e0ca6850529fe0f60c62088c
Abigael-Okemwa <abigaelbosibori2000@gmail.com>
2022-11-29 11:16:53 +0000

    combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 
 for digita in range(0, 10):
     for digitb in range(digita + 1, 10):
-        if digita == 6 and digitb == 7:
+        if digita == 8 and digitb == 9:
             print("{}{}".format(digita, digitb))
         else:
             print("{}{}".format(digita, digitb), end=", ")
