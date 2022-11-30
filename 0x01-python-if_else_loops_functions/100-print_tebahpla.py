fa64ba036623983eba601b6feef3ddba7fa7525d
Abigael-Okemwa <abigaelbosibori2000@gmail.com>
2022-11-30 07:45:30 +0000

    reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Author - Bamidele Adefolaju
 s = 0
-for d in range(ord('z'), ord('a') -1, -1):
+for d in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(d - s)), end="")
     s = 32 if s == 0 else 0
