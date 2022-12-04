c9fe36dd71186a54b905020ee8ffc7ef0d201e02
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-12-04 01:16:16 -0800

    print a list of integers

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
+
+#endif /* LISTS_H */
