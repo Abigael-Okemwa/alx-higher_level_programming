4535818daaf366b83132141e0ee293876c260add
Abigael-Okemwa <abigaelbosibori2000@gmail.com>
2022-12-05 19:24:25 +0000

    task1

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+def element_at(my_list, idx):
 def print_list_integer(my_list=[]):
 int is_palindrome(listint_t **head);
 void print_python_list_info(PyObject *p);
